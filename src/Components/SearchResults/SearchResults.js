e5bfe5c22f8359ae9725c968a4fefa973b9df70c
frivolousvision <frivolousvision@gmail.com>
2021-03-30 10:57:44 -0400

    Committing SearchResults directory

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,15 @@
+/*import { render } from '@testing-library/react';*/
+import React from 'react';
+import "../SearchResults/SearchResults.css";
+import { TrackList } from '../TrackList/TrackList';
+
+export class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+  <h2>Results</h2>
+  <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false}/>
+</div>
+        )
+    }
+}
